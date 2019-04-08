ee9cd871cc6ae2a5235895e02396b6991c47ee65
Donatas Martinkus <doncem@users.noreply.github.com>
2019-04-08 15:08:32 +0100

    Remove extra `@Component` - class is registered as bean in config

--- src/main/java/uk/gov/hmcts/reform/health/Liveness.java
+++ src/main/java/uk/gov/hmcts/reform/health/Liveness.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.health;
 
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Liveness extends AbstractHealthIndicator {
 
     @Override
